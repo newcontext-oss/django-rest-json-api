18aaf032b2ea0c1c6b1e1a0ddbae9b2c281aa5ba
Ross Patterson <me@rpatterson.net>
2017-07-30 08:10:25 -0700

    Optional nose test runner support

--- django_rest_json_api_example/settings.py
+++ django_rest_json_api_example/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+try:
+    import django_nose
+except ImportError:  # pragma: no cover
+    django_nose = None
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,3 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+if django_nose is not None:  # pragma: no cover
+    INSTALLED_APPS.append('django_nose')
+    TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
